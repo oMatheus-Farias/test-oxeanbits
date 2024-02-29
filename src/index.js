5ab900b663645db324f07a7e28ad50fa4dc59c48
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-02-29 03:14:57 -0300

    chore: initial commit with addition of initial project configuration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
-import '@progress/kendo-theme-default/dist/all.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "@progress/kendo-theme-default/dist/all.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
