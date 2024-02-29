73feaaacaa66c49f766e1169f8bff13b9028594e
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-02-29 20:57:44 -0300

    feat: add Feed Component initial config

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import "./App.css";
 
 import HeaderComponent from "./components/header/header.component";
+import FeedComponent from "./components/feed/feed.component";
 
 function App() {
   return (
     <div className="container">
       <HeaderComponent />
+      <FeedComponent />
     </div>
   );
 }
---
+++ src/components/feed/feed.component.jsx
@@ -0,0 +1,11 @@
+import styles from "./feed.module.css";
+
+const FeedComponent = () => {
+  return (
+    <div className={styles.feedContainer}>
+      <main>Page Feed</main>
+    </div>
+  );
+};
+
+export default FeedComponent;
