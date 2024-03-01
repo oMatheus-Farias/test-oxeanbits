e6e8e86de58e712f4fd8430c43f58e93063d7eaf
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-02-29 22:14:21 -0300

    feat: add Footer Component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 
 import HeaderComponent from "./components/header/header.component";
 import FeedComponent from "./components/feed/feed.component";
+import FooterComponent from "./components/footer/footer.component";
 
 function App() {
   return (
     <div className="container">
       <HeaderComponent />
       <FeedComponent />
+      <FooterComponent />
     </div>
   );
 }
---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,11 @@
+import styles from "./footer.module.css";
+
+const FooterComponent = () => {
+  return (
+    <footer className={styles.footerContainer}>
+      <p>Oxeanbits © 2024</p>
+    </footer>
+  );
+};
+
+export default FooterComponent;
