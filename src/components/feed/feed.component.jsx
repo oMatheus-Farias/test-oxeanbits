4dcf8f2c80c8aa4f80965a17890dd776b422d02b
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-02-29 21:12:55 -0300

    feat: add categories.json file

--- src/components/feed/feed.component.jsx
+++ src/components/feed/feed.component.jsx
@@ -1,6 +1,22 @@
+import { useState, useEffect } from "react";
+
 import styles from "./feed.module.css";
 
+import fechApi from "../../api/api.fetch";
+import categories from "../../api/categories.json";
+
 const FeedComponent = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const data = await fechApi();
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className={styles.feedContainer}>
       <main>Page Feed</main>
