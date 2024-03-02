287c41e48705f5d13e46e05f534f7107df85a32f
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-03-02 02:12:43 -0300

    test: ensure that data being used from the API is returned correctly

--- src/api/api.fetch.test.js
+++ src/api/api.fetch.test.js
@@ -5,3 +5,11 @@ it("should return 200", async () => {
 
   expect(response.status).toBe(200);
 });
+
+it("should check data used from the API", async () => {
+  const response = await request("https://fakestoreapi.com").get("/products");
+
+  expect(response.body[0].title).toBeDefined();
+  expect(response.body[0].price).toBeDefined();
+  expect(response.body[0].rating).toBeDefined();
+});
