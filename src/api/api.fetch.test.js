f24aa6317efb5dc72ea49569fe482c20b17c61c8
Matheus Farias <matheusfariasdasneves@gmail.com>
2024-03-03 20:19:47 -0300

    refactor: add describe to tests

--- src/api/api.fetch.test.js
+++ src/api/api.fetch.test.js
@@ -1,15 +1,17 @@
 const request = require("supertest");
 
-it("should return 200", async () => {
-  const response = await request("https://fakestoreapi.com").get("/products");
+describe("Fetch API", () => {
+  it("should return 200", async () => {
+    const response = await request("https://fakestoreapi.com").get("/products");
 
-  expect(response.status).toBe(200);
-});
+    expect(response.status).toBe(200);
+  });
 
-it("should check data used from the API", async () => {
-  const response = await request("https://fakestoreapi.com").get("/products");
+  it("should check data used from the API", async () => {
+    const response = await request("https://fakestoreapi.com").get("/products");
 
-  expect(response.body[0].title).toBeDefined();
-  expect(response.body[0].price).toBeDefined();
-  expect(response.body[0].rating).toBeDefined();
+    expect(response.body[0].title).toBeDefined();
+    expect(response.body[0].price).toBeDefined();
+    expect(response.body[0].rating).toBeDefined();
+  });
 });
